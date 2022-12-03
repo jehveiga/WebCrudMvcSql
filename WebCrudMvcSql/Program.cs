05d9fff956b36643a0e18e7f21ab021e6bb479b8
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-12-02 22:57:42 -0300

    Adicionar o Service.AddDbContext com String de conexão na Program.cs

--- WebCrudMvcSql/Program.cs
+++ WebCrudMvcSql/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebCrudMvcSql.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseSqlServer("Data Source=JEH-DESKTOP;Initial Catalog=DB_MvcSimples_Modelo;Integrated Security=True"));
 
 var app = builder.Build();
 
